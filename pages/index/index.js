2b2981dfd1e7207729341d38e516ddde23755a97
wxfeiang <wxfeiang@qq.com>
2019-07-10 15:08:30 +0800

    index页面

---
+++ pages/index/index.js
@@ -0,0 +1,191 @@
+// pages/index/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        swipers: [{
+                image: '../../images/banner1.png'
+            },
+            {
+                image: '../../images/banner2.png'
+            },
+            {
+                image: '../../images/banner3.png'
+            },
+            {
+                image: '../../images/banner4.png'
+            }
+        ],
+        classify: [{
+                image: '../../images/clessfiy1.jpg',
+                text: "水果"
+            },
+            {
+                image: '../../images/clessfiy1.jpg',
+                text: "水果"
+
+            },
+            {
+                image: '../../images/clessfiy1.jpg',
+                text: "水果"
+
+            },
+            {
+                image: '../../images/clessfiy1.jpg',
+                text: "水果"
+
+            },
+            {
+                image: '../../images/clessfiy1.jpg',
+                text: "水果"
+
+            },
+            {
+                image: '../../images/clessfiy1.jpg',
+                text: "水果"
+
+            },
+            {
+                image: '../../images/clessfiy1.jpg',
+                text: "水果"
+
+            },
+            {
+                image: '../../images/clessfiy1.jpg',
+                text: "水果"
+
+            },
+            {
+                image: '../../images/clessfiy1.jpg',
+                text: "水果"
+
+            },
+            {
+                image: '../../images/clessfiy1.jpg',
+                text: "水果"
+
+            }
+        ],
+        recommend: [{
+                title: "掌柜推荐",
+                list: [{
+                        image: "../../images/list1.jpg",
+                        title: "新疆薄皮核桃",
+                        standard: "500g",
+                        price: "￥34",
+                        price_smail: "￥00",
+                    },
+                    {
+                        image: "../../images/list1.jpg",
+                        title: "新疆薄皮核桃",
+                        standard: "500g",
+                        price: "￥34",
+                        price_smail: "￥00",
+                    },
+                    {
+                        image: "../../images/list1.jpg",
+                        title: "新疆薄皮核桃",
+                        standard: "500g",
+                        price: "￥34",
+                        price_smail: "￥00",
+                    }
+                ]
+            },
+            {
+                title: "新人推荐",
+                list: [{
+                    image: "../../images/list1.jpg",
+                    title: "新疆薄皮核桃",
+                    standard: "500g",
+                    price: "￥34",
+                    price_smail: "￥45",
+                }]
+            },
+            {
+                title: "新品手法",
+                list: [{
+                    image: "../../images/list1.jpg",
+                    title: "新疆薄皮核桃",
+                    standard: "500g",
+                    price: "￥34",
+                    price_smail: "#45",
+                }]
+            },
+            {
+                title: "新人推荐",
+                list: [{
+                    image: "../../images/list1.jpg",
+                    title: "新疆薄皮核桃",
+                    standard: "500g",
+                    price: "$34",
+                    price_smail: "#45",
+                }]
+            }
+
+        ],
+        indicatorDots: false,
+        vertical: false,
+        autoplay: true,
+        interval: 3000,
+        duration: 500
+
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
