30fb9cb46fc93cb607dea900d5c0d427e90a6f80
wxfeiang <wxfeiang@qq.com>
2019-07-12 17:22:06 +0800

    支付成功页面

---
+++ pages/success/index.js
@@ -0,0 +1,75 @@
+// pages/success/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    success_msg: {
+      image: "../../images/success.png",
+      status: "true",
+      title: "订单支付成功",
+      number: "15115648124154",
+      sum: "300.00"
+
+
+    }
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
