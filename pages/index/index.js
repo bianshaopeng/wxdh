63901661d9c46fc32334e10f31c64fe655cda650
wxfeiang <wxfeiang@qq.com>
2019-07-18 15:50:30 +0800

    加入了底部购物车

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,5 @@
 // pages/index/index.js
-var netUtil = require("../../utils/netUtils.js"); 
+var netUtil = require("../../utils/netUtils.js");
 Page({
 
   /**
@@ -10,7 +10,7 @@ Page({
     showAll: false,
     showSearch: true,
     hiddenName: false,
-    ids:["a","b","c","d"],
+    ids: ["a", "b", "c", "d"],
     scorllId: "",
     informations: {
       code: 200,
@@ -164,64 +164,79 @@ Page({
       }
 
     },
-    
 
-  
+
+
     indicatorDots: false,
     vertical: false,
     autoplay: true,
     interval: 3000,
-    duration: 500
+    duration: 500,
+
+    // 底部动画 
+    chooseSize: false,
+    animationData: {},
+    cardesc: [{
+        title: "北京方便面",
+        money: "12",
+        number: "2"
+      },
+      {
+        title: "北京方便面",
+        money: "12",
+        number: "2"
+      }
+    ]
 
   },
 
-  
-  barClick1: function() {
+
+  barClick1: function () {
     this.setData({
       scorllId: "a"
     })
 
   },
-  barClick2: function() {
+  barClick2: function () {
     this.setData({
       scorllId: "b"
     })
 
   },
-  barClick3: function() {
+  barClick3: function () {
     this.setData({
       scorllId: "c"
     })
 
   },
-  barClick4: function() {
+  barClick4: function () {
     this.setData({
       scorllId: "d"
     })
   },
-  typeClick: function(res){
+  typeClick: function (res) {
     console.log(res.currentTarget.dataset.id)
     wx.navigateTo({
       url: '../indextype/indextype?type=' + JSON.stringify(res.currentTarget.dataset.id),
     })
   },
-  imgClick:function(res){
+  imgClick: function (res) {
     console.log(res.currentTarget.dataset)
     wx.showToast({
-      title:'添加购物车成功',
+      title: '添加购物车成功',
       icon: 'success',
       duration: 2000
     })
   },
-  
-  bindinput: function(e) {
+
+  bindinput: function (e) {
     wx.navigateTo({
       url: '../search/search',
-     
+
     })
   },
-  detailsClick: function(){
-    
+  detailsClick: function () {
+
   },
   click: function (e) {
     console.log('xxx')
@@ -240,15 +255,74 @@ Page({
     })
 
   },
+  // 底部动画
+  chooseSezi: function (e) {
+    // 用that取代this，防止不必要的情况发生
+    var that = this;
+    // 创建一个动画实例
+    var animation = wx.createAnimation({
+      // 动画持续时间
+      duration: 500,
+      // 定义动画效果，当前是匀速
+      timingFunction: 'linear'
+    })
+    // 将该变量赋值给当前动画
+    that.animation = animation
+    // 先在y轴偏移，然后用step()完成一个动画
+    animation.translateY(200).step()
+    // 用setData改变当前动画
+    that.setData({
+      // 通过export()方法导出数据
+      animationData: animation.export(),
+      // 改变view里面的Wx：if
+      chooseSize: true
+    })
+    // 设置setTimeout来改变y轴偏移量，实现有感觉的滑动
+    setTimeout(function () {
+      animation.translateY(0).step()
+      that.setData({
+        animationData: animation.export()
+      })
+    }, 200)
+  },
+  hideModal: function (e) {
+    var that = this;
+    var animation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'linear'
+    })
+    that.animation = animation
+    animation.translateY(200).step()
+    that.setData({
+      animationData: animation.export()
+
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      that.setData({
+        animationData: animation.export(),
+        chooseSize: false
+      })
+    }, 200)
+  },
+
+
+
+
+
+
+
+
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
     wx.getLocation({
-      success: function(res) {
+      success: function (res) {
         console.log(res)
       },
-      fail: function(res) {
+      fail: function (res) {
         console.log(res)
       },
 
@@ -258,11 +332,11 @@ Page({
     var params = {
       Lan: "",
       Lon: 1,
-      
+
     }
 
     netUtil.getRequest(url, params, this.onStart, this.onSuccess, this.onFailed); //调用get方法情就是户数
-    
+
   },
   onStart: function () { //onStart回调
     wx.showLoading({
@@ -270,10 +344,10 @@ Page({
     })
   },
   onSuccess: function (res) { //onSuccess回调
-  console.log(res)
+    console.log(res)
     wx.hideLoading();
     this.setData({
-      informations:res
+      informations: res
       // jokeList: res.result.data //请求结果数据
     })
   },
@@ -285,7 +359,7 @@ Page({
       })
     }
   },
-  click: function(e) {
+  click: function (e) {
     this.setData({
 
       hiddenName: !this.data.hiddenName
@@ -295,49 +369,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
